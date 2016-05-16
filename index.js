c58d8302ad5be02e267cbc9a945d0d8723a01d88
Sean Duffy <seaneduffy@gmail.com>
2016-05-16 12:17:47 -0400

    updated port

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/controls', function(req, res) {
 	res.render('controls', {});
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(8880, function(){
+	console.log('listening on *:8880');
 });
 
 let comm = require('./comm/')(http, (session, label) => {
